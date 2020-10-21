653245735eebf8f31549dfa55e675531de4900f9
Ayush Govil <ayush.govil@gmail.com>
2020-10-21 19:22:54 +0530

    Added status for Bus and Flight

---
+++ src/main/java/com/mmt/services/booking/domains/BusStatus.java
@@ -0,0 +1,7 @@
+package com.mmt.services.booking.domains;
+
+public enum BusStatus {
+
+        ACTIVE,BOOKED,SCHEDULED,DELAYED,DEPARTED,ARRIVED,CANCELLED,DIVERTED,UNKNOWN
+
+}
---
+++ src/main/java/com/mmt/services/booking/domains/FlightStatus.java
@@ -0,0 +1,8 @@
+package com.mmt.services.booking.domains;
+
+public enum FlightStatus {
+
+
+        ACTIVE,BOOKED,SCHEDULED,DELAYED,DEPARTED,LANDED,IN_AIR,ARRIVED,CANCELLED,DIVERTED,UNKNOWN
+
+}
