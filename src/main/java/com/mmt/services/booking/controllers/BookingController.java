5ecfb48c7676ac03bbdc8dd0e50b10867bbf13e0
Ayush Govil <ayush.govil@gmail.com>
2020-10-21 19:31:07 +0530

    Added a new POST call to add a booking to enhance the application

--- src/main/java/com/mmt/services/booking/controllers/BookingController.java
+++ src/main/java/com/mmt/services/booking/controllers/BookingController.java
@@ -1,17 +1,16 @@
 package com.mmt.services.booking.controllers;
 
-import com.mmt.services.booking.domains.BookingDetails;
-import com.mmt.services.booking.domains.BusPriceList;
-import com.mmt.services.booking.domains.FlightPriceList;
+import com.mmt.services.booking.domains.*;
+import com.mmt.services.booking.repositories.BookingRepository;
+import com.mmt.services.booking.repositories.BusRepository;
 import com.mmt.services.booking.services.BusService;
 import com.mmt.services.booking.services.FlightService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.spring.web.json.Json;
 
 import java.util.Date;
 import java.util.List;
@@ -19,7 +18,7 @@ import java.util.List;
 @RestController
 public class BookingController {
 
-    private static final Logger LOGGER= LoggerFactory.getLogger(BookingController.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(BookingController.class);
 
     @Autowired
     FlightService flightService;
@@ -27,21 +26,38 @@ public class BookingController {
     @Autowired
     BusService busService;
 
+    @Autowired
+    BookingRepository bookingRepository;
+
     @RequestMapping("/findBookings")
     public BookingDetails findBooking(@RequestParam("source") String source, @RequestParam("destination") String destination,
                                       @RequestParam("departDate") @DateTimeFormat(pattern = "MM-dd-yyyy") Date departDate) {
 
         LOGGER.info("Inside BookingFlights() From:" + source + " TO:" + destination + "Departure Date: " + departDate);
-        List<FlightPriceList> flights=flightService.findFlights(source,destination,departDate);
-        List<BusPriceList> buses= busService.findBuses(source,destination,departDate);
+        List<FlightPriceList> flights = flightService.findFlights(source, destination, departDate);
+        List<BusPriceList> buses = busService.findBuses(source, destination, departDate);
         BookingDetails bookingDetails = new BookingDetails();
         bookingDetails.setFlights(flights);
         bookingDetails.setBuses(buses);
 
-        LOGGER.info("Flights found are"+flights.toString());
-        LOGGER.info("Buses found are"+buses.toString());
+        LOGGER.info("Flights found are" + flights.toString());
+        LOGGER.info("Buses found are" + buses.toString());
 
         return bookingDetails;
     }
 
+    @PostMapping("/addBookings")
+    public Booking addBooking(@RequestBody Passenger passenger, @RequestParam("source") String source, @RequestParam("destination") String destination, @RequestParam("flightId") int flightId, @RequestParam("busId") int busId,
+                              @RequestParam("departDate") @DateTimeFormat(pattern = "MM-dd-yyyy") Date departDate) {
+
+        LOGGER.info("Inside BookingFlights() From:" + source + " TO:" + destination + "Departure Date: " + departDate);
+
+        Passenger passengerObj = new Passenger(passenger.getPassengerId(), passenger.getEmailId(), passenger.getFirstName(), passenger.getLastName());
+        Booking bookingObj = new Booking(passengerObj.getPassengerId(), source, destination, flightId, busId, FlightStatus.BOOKED, BusStatus.BOOKED);
+        LOGGER.info("Booking Object " + bookingObj);
+        bookingRepository.save(bookingObj);
+
+        return bookingObj;
+
+    }
 }
